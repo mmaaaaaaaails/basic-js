13e8a2cda7c0752568184ad9fe2480c3717de6e9
Vadim <v.dudko93@gmail.com>
2020-02-24 21:27:43 +0300

    fix: change count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,8 +1,8 @@
 module.exports = function countCats(matrix) {
   let count = 0;
-    for (let i in matrix){
-      for (let j in matrix[i]){
-        if (j == "^^"){
+    for (let i = 0; i < matrix.length; i++){
+      for (let j = 0; j < matrix[i].length; j++){
+        if (matrix[i][j] === "^^"){
           count++;
         }
       }
